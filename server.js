7aa9c5bf611048ba224acf834af6c9cdc54ddc71
David12331 <dpantoja1@pride.hofstra.edu>
2023-12-04 09:35:26 -0500

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const methodOverride = require('method-override'); // Add this line
+const methodOverride = require('method-override'); // 
 
 require('dotenv').config()
 require('./config/database')
