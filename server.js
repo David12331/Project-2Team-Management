737c1dbcf5410699728be9c524ec4e0d04a1d6d0
David12331 <dpantoja1@pride.hofstra.edu>
2023-12-04 09:31:55 -0500

    working delete team functionality

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOverride = require('method-override'); // Add this line
 
 require('dotenv').config()
 require('./config/database')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const athletesRouter = require('./routes/athletes');  // athletes router
-const teamsRouter = require('./routes/teams');        // teams router
+const athletesRouter = require('./routes/athletes'); //athletes router
+const teamsRouter = require('./routes/teams'); // teams router
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -22,24 +22,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method')); ///////
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/athletes', athletesRouter);  // 
-app.use('/teams', teamsRouter);        // 
+app.use('/athletes', athletesRouter);
+app.use('/teams', teamsRouter);
 
-// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-  //next(createError(404));
-//});
-
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
